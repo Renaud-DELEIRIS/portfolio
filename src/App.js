a4823095ccd95711d4e0445bc460c53050b90040
Renaud-DELEIRIS <renauddeleiris@gmail.com>
2021-12-29 22:03:44 +0100

    azdded scroll view

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { Typewriter } from 'react-simple-typewriter'
+import ReactPageScroller from "react-page-scroller";
 import github from './github.svg'
 import linkedin from './linkedin.svg'
 import scroll from './scroll.svg'
@@ -8,6 +9,8 @@ import purplePolygon from './purplePolygon.png'
 import whiteCube from './whiteCube.png'
 import blackCube from './blackCube.png'
 import whiteTic from './whiteTic.png'
+import React from 'react'
+import Project from './page/Project'
 
 const Header = () => {
   return (
@@ -31,51 +34,64 @@ const Footer = () => {
 }
 
 function App() {
+  const [page, setPage] = React.useState(0)
   const redirect = (url) => {
     window.open(url, "_blank")
   }
 
+  const handlePageChange = (number) => {
+    setPage(number)
+  }
+
   return (
     <div className="App">
-      <div className='content'>
-        <Header/>
-        <Footer/>
-        <div className='container'>
-          <div className='ressource'>
-            <div className='presentation'>
-              <div className='text'>
-                <span className='hello'>Bienvenue !</span>
-                <span className='sign'>-Renaud de LEIRIS</span>
-              </div>
-              <div className='right'>
-                <span className='competence'>
-                  <Typewriter
-                    words={['Fullstack', 'C++', 'GIT']}
-                    loop={0}
-                    cursor
-                    cursorStyle='|'
-                    typeSpeed={70}
-                    deleteSpeed={50}
-                    delaySpeed={1500}
-                  />
-                </span>
+      <ReactPageScroller
+          pageOnChange={handlePageChange}
+          customPageNumber={page}
+        >
+        <div className='frontPage'>
+          <div className='content'>
+            <Header/>
+            <Footer/>
+            <div className='container'>
+              <div className='ressource'>
+                <div className='presentation'>
+                  <div className='text'>
+                    <span className='hello'>Bienvenue !</span>
+                    <span className='sign'>-Renaud de LEIRIS</span>
+                  </div>
+                  <div className='right'>
+                    <span className='competence'>
+                      <Typewriter
+                        words={['Fullstack', 'C++', 'GIT']}
+                        loop={0}
+                        cursor
+                        cursorStyle='|'
+                        typeSpeed={70}
+                        deleteSpeed={50}
+                        delaySpeed={1500}
+                      />
+                    </span>
+                  </div>
+                </div>
+                <div className='navigation'>
+                  <img alt='github' src={github} className='icon' onClick={() => {redirect("https://github.com/Renaud-DELEIRIS")}}/>
+                  <img alt='linkedin' src={linkedin} className='icon' onClick={() => {redirect("https://www.linkedin.com/in/renaud-de-leiris-505a621b3/")}}/>
+                </div>
               </div>
             </div>
-            <div className='navigation'>
-              <img alt='github' src={github} className='icon' onClick={() => {redirect("https://github.com/Renaud-DELEIRIS")}}/>
-              <img alt='linkedin' src={linkedin} className='icon' onClick={() => {redirect("https://www.linkedin.com/in/renaud-de-leiris-505a621b3/")}}/>
+            <div className='blackback'>
+              <div className='background'>
+                <img alt="purplepolygon" src={purplePolygon}/>
+                <img alt="whiteCube" src={whiteCube}/>
+                <img alt="blackCube" src={blackCube}/>
+                <img alt="whiteTic" src={whiteTic}/>
+              </div>
             </div>
           </div>
         </div>
-        <div className='blackback'>
-          <div className='background'>
-            <img alt="purplepolygon" src={purplePolygon}/>
-            <img alt="whiteCube" src={whiteCube}/>
-            <img alt="blackCube" src={blackCube}/>
-            <img alt="whiteTic" src={whiteTic}/>
-          </div>
-        </div>
-      </div>
+        <Project/>
+      </ReactPageScroller>
     </div>
   );
 }
